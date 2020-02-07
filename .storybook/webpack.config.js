fd59b4da9f697f63858f52bed84e8fbd221cbf0c
Serge M <serge@tommyjohn.com>
2020-02-07 08:29:31 -0500

    adding Storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,11 +2,16 @@ module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(js|jsx)$/,
     loader: require.resolve("babel-loader"),
-    option: {
+    options: {
       presets: [require.resolve("babel-preset-react-app")]
     }
   });
-
+  // config.plugins.push(
+  //   new StatsWriterPlugin({
+  //     filename: "stats.json"
+  //   })
+  // );
   config.resolve.extensions.push(".js", "jsx");
+
   return config;
 };
